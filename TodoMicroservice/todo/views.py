5de6a9b5b1d3daa037b45140f652ca0b601666cf
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-17 21:51:39 +0200

    adding test url path, fixing settings

--- TodoMicroservice/todo/views.py
+++ TodoMicroservice/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getRoutes(request):
+    routes = ['api/products', 'api/products/<id>']
+    return Response(routes)
