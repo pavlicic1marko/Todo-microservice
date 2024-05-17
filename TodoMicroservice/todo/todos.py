949930e2bb66442d4c27bbbc3a60abec8c0d7559
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-18 00:41:37 +0200

    adding todos.py to load from and adding urls

---
+++ TodoMicroservice/todo/todos.py
@@ -0,0 +1,47 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'Airpods Wireless Bluetooth Headphones',
+    'description':
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    'price': 89.99,
+  },
+  {
+    '_id': '2',
+    'name': 'iPhone 11 Pro 256GB Memory',
+    'description':
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    'price': 599.99,
+  },
+  {
+    '_id': '3',
+    'name': 'Cannon EOS 80D DSLR Camera',
+    'description':
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+
+    'price': 929.99,
+  },
+  {
+    '_id': '4',
+    'name': 'Sony Playstation 4 Pro White Version',
+    'description':
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+
+    'price': 399.99,
+
+  },
+  {
+    '_id': '5',
+    'name': 'Logitech G-Series Gaming Mouse',
+    'description':
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    'price': 49.99,
+  },
+  {
+    '_id': '6',
+    'name': 'take dog to the vet',
+    'description':
+      'Take max to the vet, Dr jhon Doe at 23 main street at 9 in the....',
+    'price': 12222024
+  },
+]
--- TodoMicroservice/todo/urls.py
+++ TodoMicroservice/todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('routes', views.getRoutes, name='routes'),
-
+    path('todo/products', views.getProducts, name='products'),
+    path('todo/products/<str:pk>', views.getProductsById, name='products'),
 ]
